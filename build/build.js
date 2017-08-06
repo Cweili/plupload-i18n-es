51c1491427b24a0abd8a32514ab366815d62b369
Cweili <3weili@gmail.com>
2017-08-06 14:40:24 +0800

    inital commit

---
+++ build/build.js
@@ -0,0 +1,26 @@
+const { format } = require('util');
+const { resolve } = require('path');
+const {
+  readdir,
+  readFileSync,
+  writeFileSync
+} = require('fs');
+
+const template = 'export default function(plupload) { %s }';
+
+readdir(resolve(__dirname, '../plupload/js/i18n'), (err, files) => {
+  if (err) {
+    throw err;
+  }
+  for (const file of files) {
+    writeFileSync(
+      resolve(__dirname, '../dist', file),
+      format(template, readFileSync(resolve(__dirname, '../plupload/js/i18n', file), 'utf8'))
+    );
+  }
+});
+
+writeFileSync(
+  resolve(__dirname, '../dist/package.json'),
+  readFileSync(resolve(__dirname, '../package.json'), 'utf8')
+);
